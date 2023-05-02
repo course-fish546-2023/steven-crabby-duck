22ce2c8d237a9fb67be17a5f89ab1dcb4b064206
Steven Roberts <sr320@uw.edu>
2023-05-02 12:15:26 -0700

    we 6

---
+++ code/rsync.sh
@@ -0,0 +1,25 @@
+# /home/shared/8TB_HDD_01/sr320/github$ 
+# rsync -avz . \
+# sr320@gannet.fish.washington.edu:/volume2/web/seashell/bu-github/
+
+
+  
+
+cd /home/shared/8TB_HDD_02
+rsync -avz --progress --exclude='.*' \
+skalleyc \
+zbengt \
+sarahyerrace \
+sohaught \
+bzhang11 \
+schulh2 \
+stanja \
+cnmntgna \
+sggarcia \
+cvaldi \
+jwinter2 \
+sr320@gannet.fish.washington.edu:/volume2/web/seashell/bu-546/
+
+
+
+
